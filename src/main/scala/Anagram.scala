ce5bf57088c9478e5dc2b49afa092e28313fb0c1
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-05-11 21:14:53 -0400

    clean

--- src/main/scala/Anagram.scala
+++ src/main/scala/Anagram.scala
@@ -1,17 +1,14 @@
 import scala.collection.mutable.ListBuffer
 
-//TODO : Clean code ;-)
 class Anagram(s: String) {
   def matches(strings: Seq[String]) = {
     val listBuffer = ListBuffer[String]()
-    for(chaine <- strings)
+    for(string <- strings)
     {
-      if(chaine.toLowerCase.permutations.contains(s.toLowerCase))
+      if(string.toLowerCase.permutations.contains(s.toLowerCase)
+        && !string.toLowerCase.equals(s.toLowerCase))
       {
-        if(!chaine.toLowerCase.equals(s.toLowerCase))
-        {
-          listBuffer.append(chaine)
-        }
+          listBuffer.append(string)
       }
     }
     listBuffer.toList
